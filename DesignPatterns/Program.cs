50f2fb80bd88421438617bf8921703ecc89731a7
Prakash Besra <Prakash.Besra@parentpay.com>
2023-04-08 23:39:15 +0530

    Design pattern categories added

--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace DesignPatterns
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world", args);
+        }
+    }
+}
