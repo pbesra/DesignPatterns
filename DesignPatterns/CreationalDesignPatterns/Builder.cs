e707a19b46d4f50326c4b67547e0a19420bbaf7a
Prakash Besra <Prakash.Besra@parentpay.com>
2023-04-10 00:48:01 +0530

    Builder pattern added

--- DesignPatterns/CreationalDesignPatterns/Builder.cs
+++ DesignPatterns/CreationalDesignPatterns/Builder.cs
@@ -1,12 +1,149 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DesignPatterns.CreationalDesignPatterns
+﻿namespace DesignPatterns.CreationalDesignPatterns
 {
-    internal class Builder
+    public class Builder
     {
+        public string ID { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Code { get; set; }
+        public string Address { get; set; }
+        public string DateOfBirth { get; set; }
+        public string Salary { get; set; }
+        public string IsActive { get; set; }
+        public bool HasWhere { get; set; }
+        public List<string> QueryList = new List<string>();
+
+        public Builder WithID(int id)
+        {
+            ID = $"and ID={id}";
+            QueryList.Add(ID);
+            return this;
+        }
+
+        public Builder OrWithID(int id)
+        {
+            ID = $"or ID={id}";
+            QueryList.Add(ID);
+            return this;
+        }
+
+        public Builder WithBracket(string bracket)
+        {
+            QueryList.Add(bracket);
+            return this;
+        }
+
+        public Builder WithUserName(string userName)
+        {
+            UserName = $"UserName={userName}";
+            QueryList.Add(UserName);
+            return this;
+        }
+
+        public Builder OrWithUserName(string userName)
+        {
+            UserName = $"Or UserName={userName}";
+            QueryList.Add(UserName);
+            return this;
+        }
+
+        public Builder WithEmail(string email)
+        {
+            Email = $"Email={email}";
+            QueryList.Add(Email);
+            return this;
+        }
+
+        public Builder OrWithEmail(string email)
+        {
+            Email = $"or Email={email}";
+            QueryList.Add(Email);
+            return this;
+        }
+
+        public Builder WithIsActive(bool isActive)
+        {
+            IsActive = $"and IsActive={isActive}";
+            QueryList.Add(IsActive);
+            return this;
+        }
+
+        public Builder OrWithIsActive(bool isActive)
+        {
+            IsActive = $"or IsActive={isActive}";
+            QueryList.Add(IsActive);
+            return this;
+        }
+        
+        public Builder WithJoinTable1AndTable2(string joinType, string table1, string key1, string table2, string key2)
+        {
+            QueryList.Add($" {joinType} {table1} on {table1}.{key1}={table2}.{key2}");
+            return this;
+        }
+        public Builder WithLeftJoinTable1AndTable2(string table1, string key1, string table2, string key2)
+        {
+            QueryList.Add($" left join {table1} on {table1}.{key1}={table2}.{key2}");
+            return this;
+        }
+        public Builder WithInnerJoinTable1AndTable2(string table1, string key1, string table2, string key2)
+        {
+            QueryList.Add($" join {table1} on {table1}.{key1}={table2}.{key2}");
+            return this;
+        }
+        public Builder WithFullJoinTable1AndTable2(string table1, string key1, string table2, string key2)
+        {
+            QueryList.Add($" full join {table1} on {table1}.{key1}={table2}.{key2}");
+            return this;
+        }
+
+
+        public Builder SetWhere(bool where = false)
+        {
+            HasWhere = where;
+            return this;
+        }
+
+        public Person GetPersonQuery()
+        {
+            return new Person(this);
+        }
+        
+
+        public string ToString()
+        {
+            return string.Join(' ', QueryList);
+        }
+
+        public class Person
+        {
+            public int ID { get; set; }
+            public string UserName { get; set; }
+            public string Email { get; set; }
+            public string Code { get; set; }
+            public string Address { get; set; }
+            public DateTime DateOfBirth { get; set; }
+            public int Salary { get; set; }
+            public bool IsActive { get; set; }
+            public bool HasWhere { get; set; }
+            public string QueryString { get; set; }
+
+            public string ToString()
+            {
+                return QueryString;
+            }
+
+            public Person(Builder builder)
+            {
+                QueryString = "";
+                if (!builder.HasWhere)
+                {
+                    QueryString = "Select * from Person";
+                }
+                else
+                {
+                    QueryString = $"Select * from Person Where {builder.ToString()}";
+                }
+            }
+        }
     }
 }
--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -8,10 +8,18 @@ namespace DesignPatterns
     {
         public static void Main(string[] args)
         {
-            var abstractFactory = new AbstractFactory()
-                .GenerateFactory(FactoryType.Laptop)
-                .CreateDevice(DeviceType.DELL);
-            Console.WriteLine(abstractFactory.GetDetails());
+            var builder=new Builder();
+            var personQuery = builder
+                .SetWhere(true)
+                .WithBracket("(")
+                .WithEmail("psbesra@gmail.com")
+                .OrWithID(10)
+                .WithIsActive(true)
+                .WithBracket(")")
+                .WithLeftJoinTable1AndTable2("Person", "ID", "Organization", "PersonID")
+                .GetPersonQuery();
+
+            Console.WriteLine(personQuery.ToString());
         }
     }
 }
